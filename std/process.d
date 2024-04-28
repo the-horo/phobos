eb968d505402f041ad7059a5893c4d8c2ca06a43
Andrea Fontana <trikkuz@gmail.com>
2024-04-29 00:03:19 +0200

    Euristic selection of closing method
    Co-authored-by: Andrei Horodniceanu <a.horodniceanu@proton.me>

--- std/process.d
+++ std/process.d
@@ -1010,6 +1010,7 @@ private Pid spawnProcessPosix(scope const(char[])[] args,
             close(pidPipe[0]);
         close(forkPipe[0]);
         auto forkPipeOut = forkPipe[1];
+        immutable highestOpenFileDescriptorEstimate = forkPipeOut;
         immutable pidPipeOut = pidPipe[1];
 
         // Set the working directory.
@@ -1051,6 +1052,7 @@ private Pid spawnProcessPosix(scope const(char[])[] args,
                 static if (!__traits(compiles, closefrom))
                 {
                     void fallback (int lowfd) {
+                        import core.sys.posix.sys.resource : rlimit, getrlimit, RLIMIT_NOFILE;
                         import core.sys.posix.dirent : dirent, opendir, readdir, closedir, DIR;
                         import core.sys.posix.unistd : close;
                         import core.sys.posix.stdlib : atoi, malloc, free;
@@ -1059,9 +1061,23 @@ private Pid spawnProcessPosix(scope const(char[])[] args,
                         pragma(mangle, "dirfd")
                         extern(C) nothrow @nogc int dirfd(DIR* dir);
 
-                        // Try to open the directory /dev/fd or /proc/self/fd
-                        DIR* dir = opendir("/dev/fd");
-                        if (dir is null) dir = opendir("/proc/self/fd");
+                        // Get the maximum number of file descriptors that could be open.
+                        rlimit r;
+                        if (getrlimit(RLIMIT_NOFILE, &r) != 0)
+                            abortOnError(forkPipeOut, InternalError.getrlimit, .errno);
+
+                        DIR* dir = null;
+
+                        // Reading /dev/fd or /proc/self/fd works better than using poll() if ...
+                        if (
+                            r.rlim_cur/(highestOpenFileDescriptorEstimate+1) > 120 || // ... the number of file descriptors is small ...
+                            r.rlim_cur > 1024*1024                                    // ... or the soft limit is high. In this case poll would allocate a huge array)
+                        )
+                        {
+                            // Try to open the directory /dev/fd or /proc/self/fd
+                            dir = opendir("/dev/fd");
+                            if (dir is null) dir = opendir("/proc/self/fd");
+                        }
 
                         if (dir)
                         {
@@ -1090,14 +1106,7 @@ private Pid spawnProcessPosix(scope const(char[])[] args,
                             // in its own implementation via opendir().
                             import core.stdc.stdlib : malloc;
                             import core.sys.posix.poll : pollfd, poll, POLLNVAL;
-                            import core.sys.posix.sys.resource : rlimit, getrlimit, RLIMIT_NOFILE;
 
-                            // Get the maximum number of file descriptors that could be open.
-                            rlimit r;
-                            if (getrlimit(RLIMIT_NOFILE, &r) != 0)
-                            {
-                                abortOnError(forkPipeOut, InternalError.getrlimit, .errno);
-                            }
                             immutable maxDescriptors = cast(int) r.rlim_cur;
                             immutable maxToClose = maxDescriptors - lowfd;
 
